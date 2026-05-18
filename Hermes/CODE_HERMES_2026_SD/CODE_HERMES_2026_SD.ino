930bf2d0b082560ce5e658bdbd927975c338e708
CedricChauvet <chauvetc.ujf@gmail.com>
2026-05-18 13:26:04 +0200

    refactor du code principal

--- Hermes/CODE_HERMES_2026_SD/CODE_HERMES_2026_SD.ino
+++ Hermes/CODE_HERMES_2026_SD/CODE_HERMES_2026_SD.ino
@@ -1,6 +1,7 @@
 /* CODE HERMES 2026 course avec 3 lasers utilisés, avec frein */
 
-#include "Servo.h"
+
+/****** initialisation SD ******/
 #include <SD.h> 
 // variables pour lecteur carte SD.
 // Nom du fichier dans lequel écrire
@@ -10,6 +11,8 @@ File txtFile;
 // chaîne pour mettre en mémoire tampon la sortie
 String buffer;
 
+
+/****** initialisation TFMiniPlus ******/
 #include <TFMPlus.h>
 TFMPlus tfmP1;         // Creation objet TFMini Avant Gauche
 TFMPlus tfmP2;        // Creation objet TFMini Avant Droit
@@ -17,21 +20,25 @@ TFMPlus tfmP3;        // Creation objet TFMini Frontal Gauche
 TFMPlus tfmP4;        // Creation objet TFMini Frontal Droit
 TFMPlus tfmP5;        // Creation objet TFMini Frontal Central
                   
-String chaine = " ";                          // chaine de caractère contenant le message 
-Servo myservo;                       // déclaration objet servo
-const int pinTpntH=12;                // pin pont en H forward 
-const int pinTpntHrev=11;             // pin pont en H reverse
-const int pinServo=5;                 // pin servo direction  
-const int pinOdo= 3;                   // pin odométrie
-int angleDefault=100;                  // variable de commande servo direction à braquage nul
-int angleBraq=angleDefault;           // variable commande braquage
-
 bool idLA=false;          // flag laser a transmis données
 bool idLB=false;
 bool idLC=false;
 bool idLD=false;
 bool idL5=false;
-unsigned long lastMsg = 0;
+
+
+#include "Servo.h"
+Servo myservo;                       // déclaration objet servo
+const int pinServo=5;                // pin servo direction
+const int pinTpntH=12;                // pin pont en H forward 
+const int pinTpntHrev=11;             // pin pont en H reverse
+                   
+const int pinOdo= 3;                   // pin odométrie
+long int HallMil = 1;                  // temps exact au passage devant le capteur 
+int angleBraq=100;           // variable commande braquage
+
+
+
                     // variables odométrie 
 unsigned long t;                                        // délai entre deux lecture du pignon pour odométrie
 unsigned long nbPignon=0;                               // cumul du nbre de tours roue
@@ -71,35 +78,12 @@ bool neutre = false;                              // indicateur état précéden
 int minDist=300;                                   // distance mini capteur frontal déclenchant le freinage
 int PwmReverse=180;                                 // valeur du Pwm reverse
 int PwmFrein = PwmReverse;
-float consiTroncon =0.0;                          // consigne vitesse donnée par la table troncon
-float consi=0.0;                                  // consigne vitesse effective
-float consiVite =15.0;                               // consigne vitesse ligne droite
-float consiLent =9.0;                               // consigne vitesse virages
-long int t0frein =0;                              // compteur millis pour intervalle coup de frein
-long int dfrein=5;                                // durée action freinage en ms.
-bool debFrein = false;                            // indicateur première boucle de freinageconsi=consiVite;
+
+
 float dstart=150.0;
 float tprec=0.0;
 float errPrec=0.0;
-float tronCon[16][2] {         // tableau 16 tronçons : cm et km/h 
-            {0,5},                        // 
-            {50,10},                       // 
-            {100,10},                      // 
-            {500,10},                     // 6m: premier portique
-            {3000,0},                     //  Charbonnière     
-            {13031,0},                     // portique          
-            {5462,0},                     // Portique  
-            {5562,0},                     //
-            {7893,0},                     // Portique
-            {8093,0},                     //
-            {10324,0},                    // Portique
-            {10524,0},
-            {12754,0},                    // Dernier Portique
-            {13050,0},                    // fin de course
-            {132000,0},                                                
-            {999999,0}                    
- };
- int lgTroncon =(sizeof(tronCon)/sizeof(tronCon[0]))-1;                    
+float erreur = 0.0;
                       // Acquisitions mesures capteurs laser
 int AVG;                                         //mesure du laser AV G
 int AVD;          //mesure du laser AV droit
@@ -108,21 +92,7 @@ int FG;           //mesure du laser frontal gauche
 int FD;           //mesure du laser frontal droite
 int check;        //variable de contrôle checksum tampon données laser
 int i;
-const int HEADER=0x59;          //entête tampon données laser
-                                // variables spécifiques au pilotage lasers droite
-float l=19.5;                                      // distance en cm entre les deux capteurs lasers latéraux droite
-float erreur;                                     // erreur totale (erreur alpha + erreur distance)
-float lastErreur;
-float alpha=0.0;                                  // angle alpha si deux lasers latéraux
-//float consigneD=50.0;                             // consigne de suivi de la bordure à la distance D
-float consigneAlpha=0.;                           // consigne de suivi de la bordure avec un angle Alpha (= 0 en ligne droite)
-float lastAlpha=0.0;                              // erreur précédente pour calculer le delta alpha
-float lastDist=0.0;                               // erreur précédente pour calculer le delta distance
-                                //Coefficients pilotage lasers latéraux
-float K1 = 0.5;                                    // coefficient erreur alpha
-float K2 = 0.7;                                   // coefficient erreur distance
-float KD1 = 0.0;                                  // coefficient delta erreur alpha
-float KD2 = 0.0;                                  // coefficient delta erreur distance
+
                               // compteurs de temps
 long int t0;                                      // valeur micros() en début de boucle
 long int t1;                                     
@@ -160,12 +130,12 @@ void loop() {
   //litLaser('E');                   // appel lecture laser frontal C:FC
 compteur();
 ouEstil();
+vcible = computeSpeed(FC);
 if(FC<dstart){
- needFrein();
+ needFrein(vcible);
 }
 else{
- asservVITPID(VIT,consi);
- motor();
+ asservVITPID(VIT,vcible);
 }
 //debug();     
 Chargebuffer();                                 // appel fonction de chargement du buffer pour écriture sur SD
@@ -185,69 +155,83 @@ void movServo(){
 }
 
 void compteur(){
-deltaNbPignon = nbPignon - lastNbPignon;
-if(deltaNbPignon >0){
-deltaMicro = micros() - lastMicro;
-lastNbPignon = nbPignon;
-lastMicro = micros();
-nb_tour_sec= deltaNbPignon/(deltaMicro/1000000.0)/2.;
-VIT = (nb_tour_sec*3600)*(3.1416*7.2/100000);
-if(VIT <= 1.0){
-  VIT = 1.0;
+
+  deltaNbPignon = nbPignon - lastNbPignon;
+  if(deltaNbPignon >0){
+    deltaMicro = micros() - lastMicro;
+    lastNbPignon = nbPignon;
+    lastMicro = micros();
+    nb_tour_sec= deltaNbPignon/(deltaMicro/1000000.0)/2.;
+    VIT = (nb_tour_sec*3600)*(3.1416*7.2/100000);
+  }
+  if (lastMicro - HallMil > 500) VIT =0;
 }
 
-cumDist = (nbPignon)*3.1416*7.2;
-}
-}
 
 void ouEstil() {
   cumDist = (nbPignon)*3.1416*7.2/2;      // en cm, diam roue 7.2, 2 aimants,calcul cumul distance 
-  for (int i=0; i <= 8;i++){ 
-
-    if(cumDist >= tronCon[i][0] and cumDist < tronCon[i+1][0]){
-      consi = tronCon[i][1];
+  
+  // Freinage a partir d'une distance parcourue
+  if (cumDist>10000){
+    while(1){
+        analogWrite(pinTpntH,0);
+        analogWrite(pinTpntHrev,0);
+        delay(10000);
     }
   }
 }
 
-void needFrein(){
-unsigned long tdeb= millis();
-float vmin=6.0;
-float dstart=150.0;
-float dend=60.0;
-vcible = max(vmin + (consi-vmin)*((FC-dend)/(dstart-dend)), vmin);
-consi = vcible;
-float err= vcible-VIT;
-unsigned long tnow=millis();
-float dt=(tnow-tprec)/1000.0;
-float deriv=(err-errPrec)/dt;
-float P=10*err;
-float D=0.5*deriv;
-pid=P+D;
-errPrec=err;
-tprec=tnow;
-pid=constrain(pid,-200,200);
-PwmVIT=pid;
-if(pid>0)
-{
-  analogWrite(pinTpntH,pid);
-  analogWrite(pinTpntHrev,0);
-}
-else
-{
-  analogWrite (pinTpntH,0);
-  analogWrite(pinTpntHrev,-pid);
-}
+
+float computeSpeed(float dist){
+  float vmax=15.0 ;
+  float vmin=6.0 ;
+  float dstart=150.0 ;
+  float dend=50.0 ;
+  if(dist>dstart) return vmax;
+  if(dist<dend) return vmin;
+  float ratio= (FC-dend)/(dstart-dend);
+
+  return vmin+(vmax-vmin)*ratio;
 }
 
 
-void asservVITPID(float VIT, float vitesseConsigne) {
+void needFrein(float vcible){
+  unsigned long tdeb= millis();
+  float vmin=6.0;
+  float dstart=150.0;
+  float dend=60.0;
+
+  float err= vcible-VIT;
+  unsigned long tnow=millis();
+  float dt=(tnow-tprec)/1000.0;
+  float deriv=(err-errPrec)/dt;
+  float P=10*err;
+  float D=0.5*deriv;
+  pid=P+D;
+  errPrec=err;
+  tprec=tnow;
+  pid=constrain(pid,-200,200);
+  PwmVIT=pid;
+  if(pid>0)
+  {
+    analogWrite(pinTpntH,pid);
+    analogWrite(pinTpntHrev,0);
+  }
+  else
+  {
+    analogWrite (pinTpntH,0);
+    analogWrite(pinTpntHrev,-pid);
+  }
+}
+
+
+void asservVITPID(float VIT, float vcible) {
                         // Temps écoulé
   unsigned long tNow = millis();
   float dt = (tNow - tPrec_v) / 1000.0;  // secondes
   if (dt < 0.001) dt = 0.001;
                       // Erreur instantanée
-  float err = vitesseConsigne - VIT;
+  float err = vcible - VIT;
                       // PID
   integrale_v += err * dt;
   float derivee = (err - erreurPrec_v) / dt;
@@ -263,22 +247,19 @@ void asservVITPID(float VIT, float vitesseConsigne) {
                   // Lissage possible pour éviter à-coups
   float alpha = 0.7;
   commandePWM_v = alpha * commandePWM_v + (1 - alpha) * sortie;
-  if(consi>0){
-  PwmVIT = commandePWM_v;
-  }else {
-  PwmVIT = 0;
-  }
-}
-
-void motor(){
+  
   PwmVIT = constrain(PwmVIT, 0,80);
   analogWrite(pinTpntH,PwmVIT);
- }
-
-void countInterrupt(){                // fonction appelée par l'attach interrupt
-nbPignon++;
 }
 
+
+void countInterrupt(){                // fonction appelée par l'attach interrupt
+  nbPignon++;
+  HallMil = millis();
+}
+
+
+/**********************    PARTIE TFMiniPlus    **********************/ 
 
 void initSerialTfminiPlus(){      // Initialisation des ports série et des objets tfminiPlus
   Serial1.begin(115200); 
@@ -481,7 +462,7 @@ void litLaser(char car){     // Lecture des lasers
 }
 
 
-
+/**********************    PARTIE Carte SD    **********************/ 
 
 void SdSetup(){               // fonction initialisation carte SD
   delay(1000);
@@ -505,7 +486,7 @@ void SdSetup(){               // fonction initialisation carte SD
   Serial.println("Fichier créé et ouvert !");
   // Ajout ligne entête fichier
   txtFile.println();
-  txtFile.println("Durée;Distance;PWM;CONSIGNE;VITESSE;laser_FC");
+  txtFile.println("Durée;Distance;PWM;Vit Cible;VITESSE;laser_FC");
   txtFile.flush();
   }
 
@@ -519,7 +500,7 @@ void SdSetup(){               // fonction initialisation carte SD
     buffer += ";";
     buffer += PwmVIT;
     buffer += ";";
-    buffer += consi;
+    buffer += vcible;
     buffer += ";";
     buffer += VIT;
     buffer += ";";
